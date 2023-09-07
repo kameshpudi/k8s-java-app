689089f143a36b8787abddaf794a0ed53772d8ff
Rao pudi Kameswara <kameswara-rao.pudi@partner.bmwgroup.com>
2023-09-07 22:49:38 +0200

    adding params to java function

--- src/main/java/io/jaylee/springboot/Application.java
+++ src/main/java/io/jaylee/springboot/Application.java
@@ -6,6 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 @GeneratorOptions(outputPath = "/")
