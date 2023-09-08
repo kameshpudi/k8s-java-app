fff1ded55d99a68b4baee3ebdcdc6034313ca16a
Rao pudi Kameswara <kameswara-rao.pudi@partner.bmwgroup.com>
2023-09-08 08:00:28 +0200

    image test

--- src/main/java/io/jaylee/springboot/Application.java
+++ src/main/java/io/jaylee/springboot/Application.java
@@ -6,6 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 @GeneratorOptions(outputPath = "/")
@@ -20,7 +22,8 @@ public class Application {
 class HelloController {
 
 	@GetMapping("/api/v1/hello")
-	public String hello() {
-		return "Hello World!";
+	@ResponseBody
+	public String hello(@RequestParam(name="name", required=false, defaultValue="Carl Benz") String name) {
+		return "Hello, did you know that " + name + "invented the car?";
 	}
 }
